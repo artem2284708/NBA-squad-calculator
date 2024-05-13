8cce16a4b2f056aa3741f701731cd3e64a438a21
artem2284708 <artemmmmkk@gmail.com>
2024-05-14 01:26:35 +0300

    Для первой таблици избегаем ошибки со стартовой пятёркой (каждый игрок должен быть со своей позиции)

--- main.py
+++ main.py
@@ -5,12 +5,24 @@ advanced = pd.read_csv('~/PycharmProjects/NbaSquadGenerator/File.csv/Advanced.cs
 salary = pd.read_excel('~/PycharmProjects/NbaSquadGenerator/File.csv/NBA Player Contracts.xlsx')
 new_column_names = ['Rk', 'player', 'Tm', '2023-24', '2024-25', '2025-26', '2026-27', '2027-28', '2028-29', 'Guaranteed']
 salary.columns = new_column_names
+
 salary = salary[1:500]
+'''def convert_dollar_to_int(s):
+    if isinstance(s, str):
+        return int(s.replace("$", "").replace(",", ""))
+    else:
+        return 0
+
+salary['2023-24'] = salary['2023-24'].apply(convert_dollar_to_int)
+salary['2024-25'] = salary['2024-25'].apply(convert_dollar_to_int)
+salary['2025-26'] = salary['2025-26'].apply(convert_dollar_to_int)
+salary['2026-27'] = salary['2026-27'].apply(convert_dollar_to_int)
+salary['2027-28'] = salary['2027-28'].apply(convert_dollar_to_int)
+salary['Guaranteed'] = salary['Guaranteed'].apply(convert_dollar_to_int)'''
 
 curAdvanced = advanced.loc[advanced['season'] == 2024]  # применяю фильтр на текущий год
 
 total = pd.merge(curAdvanced, salary, on='player', how='inner').drop(columns='birth_year')
-
 
 
 team = input().upper()  # тут человек пишет название команды по которой ведёт поиск
@@ -42,6 +54,8 @@ selected_players_index = squad.index.tolist()
 curTeam_updated = mp_team.drop(index=selected_players_index)
 
 squad = squad._append(curTeam_updated)
+output_Table1 = squad.to_string(index=False)
 
-print(squad)
+
+print(output_Table1)
 
