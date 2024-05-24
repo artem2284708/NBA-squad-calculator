04ea3341de644b78b5afcb16bd6d4565e1ad4f79
artem2284708 <145186264+artem2284708@users.noreply.github.com>
2024-05-24 12:41:51 +0300

    Update form.cpp
    We running Python script
    
    Signed-off-by: artem2284708 <145186264+artem2284708@users.noreply.github.com>

--- NBA_squad/form.cpp
+++ NBA_squad/form.cpp
@@ -6,6 +6,16 @@ Form::Form(QWidget *parent)
     , ui(new Ui::Form)
 {
     ui->setupUi(this);
+
+
+    QProcess *process = new QProcess(parent);
+    process->start("python3", {"/Users/artem2284708/PycharmProjects/NbaSquadGenerator/Table preprocessing/Get results.py"});
+    process->waitForFinished();
+    process->write("den");
+    process->closeWriteChannel();
+    process->waitForFinished();
+    qDebug() << process->readAllStandardOutput();
+    delete process;
 }
 
 Form::~Form()
